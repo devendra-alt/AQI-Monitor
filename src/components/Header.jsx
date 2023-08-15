18392dc0e8ef94d465e08d0f9f0b3cd67991ec6c
devendra-alt <devendramulewa171@gmail.com>
2023-08-15 21:07:19 +0530

    Add title and setting button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import settings from '../assets/settings.svg';
+import back from '../assets/back.svg';
 
-function Header() {
-  return <div>Header</div>;
+function Header({ title }) {
+  return (
+    <header className="main-header">
+      <div className="main-left-back-area">
+        {title !== 'AQI monitor' && (
+          <button className="back-btn btn" type="button">
+            <img
+              src={back}
+              width="20px"
+              height="20px"
+              alt="back icon"
+              className="back-img"
+            />
+          </button>
+        )}
+      </div>
+      <h1 className="main-title">{title}</h1>
+      <div className="main-utils">
+        <button className="settings btn" type="button">
+          <img
+            src={settings}
+            width="20px"
+            height="20px"
+            alt="settings icon"
+            className="setting-img"
+          />
+        </button>
+      </div>
+    </header>
+  );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
