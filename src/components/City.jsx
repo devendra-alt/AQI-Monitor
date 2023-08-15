0a3d9edcfeef648d70f9ec789868059e0e5f9f01
devendra-alt <devendramulewa171@gmail.com>
2023-08-15 21:07:02 +0530

    Add City listElement

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import millify from 'millify';
+import right from '../assets/right.svg';
 
-function City() {
-  return <div>City</div>;
+function City({ city, country, population }) {
+  return (
+    <li className="city-list-item">
+      <p className="city-name">
+        {city}
+        <br />
+        <span>City</span>
+      </p>
+      <p className="country-name">
+        {country}
+        <br />
+        <span>Country</span>
+      </p>
+      <p className="population">
+        {millify(population)}
+        <br />
+        <span>Population</span>
+      </p>
+      <button className="see-more btn" type="button">
+        See AQI
+        <img src={right} width="20px" height="20px" alt="right arrow" />
+      </button>
+    </li>
+  );
 }
+
+City.defaultProps = {
+  city: 'no data',
+  country: 'no data',
+  population: 'no data',
+};
+
+City.propTypes = {
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  population: PropTypes.number.isRequired,
+};
 
 export default City;
