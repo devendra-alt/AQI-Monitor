6c9d051c6afc374bfb44d82c98f5a1fb09c5e4fd
devendra-alt <devendramulewa171@gmail.com>
2023-08-15 21:04:53 +0530

    Update App.js dispatch effect

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { useDispatch } from 'react-redux';
 import Home from './components/Home';
 import Country from './components/Country';
 import City from './components/City';
-import { fetchCountries } from './redux/features/countries';
 import { fetchCitys } from './redux/features/citys';
-import { fetchAqiData } from './redux/features/aqi';
 
 const router = createBrowserRouter([
   {
@@ -26,13 +24,7 @@ const router = createBrowserRouter([
 function App() {
   const dispath = useDispatch();
   useEffect(() => {
-    dispath(fetchCountries());
     dispath(fetchCitys());
-    const obj = {
-      lat: 28.61,
-      lon: 77.23,
-    };
-    dispath(fetchAqiData(obj));
   }, [dispath]);
   return <RouterProvider router={router} />;
 }
