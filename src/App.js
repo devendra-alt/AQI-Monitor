486b69f62f0717dc2014fdb5dd6d949a9bbd06fe
devendra-alt <devendramulewa171@gmail.com>
2023-08-15 11:03:19 +0530

    Add useDispatch() for fetching aqi data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Home from './components/Home';
 import Country from './components/Country';
 import City from './components/City';
+import { fetchCountries } from './redux/features/countries';
+import { fetchCitys } from './redux/features/citys';
+import { fetchAqiData } from './redux/features/aqi';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +24,16 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const dispath = useDispatch();
+  useEffect(() => {
+    dispath(fetchCountries());
+    dispath(fetchCitys());
+    const obj = {
+      lat: 28.61,
+      lon: 77.23,
+    };
+    dispath(fetchAqiData(obj));
+  }, []);
   return <RouterProvider router={router} />;
 }
 
